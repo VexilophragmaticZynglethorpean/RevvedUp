311e51a9cfc94700273997b871232f8b10202d93
VexilophragmaticZynglethorpean <prasiddha.7941@proton.me>
2024-11-12 19:09:18 +0000

    Delete build directory

--- build/external/SFML/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.o.d
+++
@@ -1,6 +0,0 @@
-external/SFML/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.o: \
- /home/amnesia/Persistent/RevvedUp/external/SFML/src/SFML/Graphics/BlendMode.cpp \
- /usr/include/stdc-predef.h \
- /home/amnesia/Persistent/RevvedUp/external/SFML/include/SFML/Graphics/BlendMode.hpp \
- /home/amnesia/Persistent/RevvedUp/external/SFML/include/SFML/Graphics/Export.hpp \
- /home/amnesia/Persistent/RevvedUp/external/SFML/include/SFML/Config.hpp
--- build/external/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Sleep.cpp.o.d
+++
@@ -1,8 +0,0 @@
-external/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Sleep.cpp.o: \
- /home/amnesia/Persistent/RevvedUp/external/SFML/src/SFML/System/Sleep.cpp \
- /usr/include/stdc-predef.h \
- /home/amnesia/Persistent/RevvedUp/external/SFML/include/SFML/System/Sleep.hpp \
- /home/amnesia/Persistent/RevvedUp/external/SFML/include/SFML/System/Export.hpp \
- /home/amnesia/Persistent/RevvedUp/external/SFML/include/SFML/Config.hpp \
- /home/amnesia/Persistent/RevvedUp/external/SFML/include/SFML/System/Time.hpp \
- /home/amnesia/Persistent/RevvedUp/external/SFML/src/SFML/System/Unix/SleepImpl.hpp
