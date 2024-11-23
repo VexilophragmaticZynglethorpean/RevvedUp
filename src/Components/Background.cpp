a1e3acf8bb3138b27ce67f75570fc748f1458d8c
VexilophragmaticZynglethorpean <VexilophragmaticZynglethorpean@github.com>
2024-11-23 20:54:26 +0000

    Fixed sth

--- src/Components/Background.cpp
+++ src/Components/Background.cpp
@@ -19,11 +19,11 @@ void
 Background::init()
 {
     atlases.at(static_cast<std::size_t>(TextureID::ATLAS0)) =
-      Atlas(4, 6, 23, 500.0);
-    atlases.at(static_cast<std::size_t>(TextureID::ATLAS1)) =
-      Atlas(4, 6, 23, 50.0);
-    atlases.at(static_cast<std::size_t>(TextureID::ATLAS2)) =
       Atlas(5, 11, 55, 100.0);
+    atlases.at(static_cast<std::size_t>(TextureID::ATLAS1)) =
+      Atlas(4, 6, 23, 2.0);
+    atlases.at(static_cast<std::size_t>(TextureID::ATLAS2)) =
+      Atlas(4, 6, 23, 500.0);
 
     changeAtlas(currentTexture);
 }
